27cd5e6a22f9da5440328a441adb70934f4f1887
tharunmadhiyazhagan <tharun.madhiyazhagan@gmail.com>
2025-03-04 00:01:25 -0500

    HomePage initially is done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,26 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Tharun" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 export default Home;
